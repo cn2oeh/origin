94f3fdb17f5b5452762239c407b5d42ea5ec74c8
cn2oeh <cn2oeh@neptun.gde.hu>
2023-12-09 17:20:47 +0100

    commmit

--- poligon.py
+++ poligon.py
@@ -8,8 +8,5 @@ class Poligon:
 my_poligon = Poligon(4, "zöld")
 print(f"Ennek a poligonnak {my_poligon.sides} oldala van és a színe {my_poligon.color} és az oldalszélessége {my_poligon.oldalszelesseg}.")
 
-your_poligon = Poligon(3, "kék")
-print(f"Ennek a poligonnak {your_poligon.sides} oldala van és a színe {your_poligon.color} és az oldalszélessége {your_poligon.oldalszelesseg}.")
-
-third_poligon = Poligon(5)
-print(f"Ennek a poligonnak {third_poligon.sides} oldala van és a színe {third_poligon.color} és az oldalszélessége {third_poligon.oldalszelesseg}.")
+my_poligon.sides = 3
+print(f"Ennek a poligonnak {my_poligon.sides} oldala van és a színe {my_poligon.color} és az oldalszélessége {my_poligon.oldalszelesseg}.")
