f0c4eee199b250bd6116b2b4025efd1c8e294ab0
cn2oeh <cn2oeh@neptun.gde.hu>
2023-12-09 17:05:04 +0100

    negyedik commmit

--- poligon.py
+++ poligon.py
@@ -4,3 +4,14 @@ class Poligon:
 
 class Triangle:
     pass
+
+
+my_poligon = Poligon()
+
+your_poligon = Poligon()
+
+print(my_poligon)
+print(id(my_poligon))
+
+print(your_poligon)
+print(id(your_poligon))
