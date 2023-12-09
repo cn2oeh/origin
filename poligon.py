561ca0cb4c14e5af98b2bda7d78f5f2bb1f5cf6d
cn2oeh <cn2oeh@neptun.gde.hu>
2023-12-09 20:58:06 +0100

    commmit

--- poligon.py
+++ poligon.py
@@ -2,17 +2,16 @@ class Poligon:
     color = "piros"
     def __init__(self, sides):
         self.sides = sides
-        self.oldalszelesseg = 2
+        self._oldalszelesseg = 2
+
+    def getoldalszelesseg(self):
+        return self._oldalszelesseg
+
+    def __str__(self):
+        return "Én egy poligon vagyok."
 
 
 my_poligon = Poligon(4)
-print(f"Ennek a poligonnak {my_poligon.sides} oldala van és a színe {my_poligon.color}.")
 
-my_triangle = Poligon(3)
-print(f"Ennek a poligonnak {my_triangle.sides} oldala van és a színe {my_triangle.color}.")
-
-Poligon.color="kék"
-print(f"Ennek a poligonnak {my_triangle.sides} oldala van és a színe {my_triangle.color}.")
-
-print(f"My_poligon poligonnak {my_poligon.sides} oldala van és a színe {my_triangle.color}.")
-
+print(my_poligon.getoldalszelesseg())
+print(my_poligon)
