9e2238a77c418e5f24e79ff63e12388f789ad109
cn2oeh <cn2oeh@neptun.gde.hu>
2023-12-09 21:49:25 +0100

    commmit

--- auto.py
+++ auto.py
@@ -3,16 +3,16 @@ class Auto:
     def __init__(self):
         self._sebesseg = 0
 
-
-    def get_sebesseg(self):
+    @property
+    def sebesseg(self):
         print("Ez a getter.")
         return self._sebesseg
 
-    def set_sebesseg(self, ujsebesseg):
+    @sebesseg.setter
+    def sebesseg(self, ujsebesseg):
         print("Ez a setter.")
         self._sebesseg = ujsebesseg
 
-    sebesseg = property(get_sebesseg, set_sebesseg)
 
 my_auto = Auto()
 
