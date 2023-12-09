74f9a90d69f2b9a83665b292ac91625cc43a5810
cn2oeh <cn2oeh@neptun.gde.hu>
2023-12-09 21:28:06 +0100

    commmit

--- wolt2.py
+++ wolt2.py
@@ -1,8 +1,26 @@
 class Etel:
 
-    def __init__(self, nev, ar):
-        self.nev = nev
-        self.ar = ar
+    def __init__(self):
+        self._nev = ""
+        self._ar = 0
+
+    def get_ar(self):
+        return self._ar
+
+    def set_ar(self, ar):
+        if ar < 0 or ar > 100000:
+            print("Nem megfelelő ár!")
+        else:
+            self._ar = ar
+
+
+    def get_nev(self):
+        return self._nev
+
+
+    def set_nev(self, ujnev):
+        self._nev = ujnev
+
 
 class Restaurant:
     def __init__(self, menuitems, etteremnev):
@@ -10,30 +28,26 @@ class Restaurant:
         self.etteremnev = etteremnev
 
     def __str__(self):
-        return f"Az éltterem neve {self.etteremnev}"
+        return f"Az étterem neve {self.etteremnev}"
 
 
     def __add__(self, other):
         self.menuitems.append(other)
     def getmenuitems(self):
         for menuitem in self.menuitems:
-            print(f"{menuitem.nev}................{menuitem.ar} Ft")
+            print(f"{menuitem.get_nev()}................{menuitem.get_ar()} Ft")
 
-etel1 = Etel('Hambi', 100)
-etel2 = Etel('Hotdog', 150)
-my_menu = [etel1, etel2]
-
-my_restaurant = Restaurant(my_menu,"Kisbojtár")
+my_restaurant = Restaurant([],"Kisbojtár")
 my_restaurant.getmenuitems()
 
 for i in range(2):
     etel_neve = input("Add meg az étel nevét: ")
     etel_ara = int(input("Add meg az étel árát: "))
-    if etel_ara < 0 or etel_ara > 100000:
-        print("Nem megfelelő ár!")
-        break
-    else:
-        my_restaurant + Etel(etel_neve, etel_ara)
+    etel_uj = Etel()
+    etel_uj.set_nev(etel_neve)
+    etel_uj.set_ar(etel_ara)
+    my_restaurant + etel_uj
+
 
 my_restaurant.getmenuitems()
 
